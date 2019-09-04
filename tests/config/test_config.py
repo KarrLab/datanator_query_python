653815d66cd8e96a33fc25a83c678df3fcf934ba
Zhouyang Lian <zhouyang.lian@familian.life>
2019-09-04 14:53:48 -0400

    test_config.py

--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -8,6 +8,7 @@ class TestConfig(unittest.TestCase):
 	def setUpClass(cls):
 		cls.src_basic = config.Config()
 		cls.src_prod = config.ProductionConfig()
+		cls.src_test = config.TestConfig()
 
 	@classmethod
 	def tearDownClass(cls):
@@ -21,3 +22,9 @@ class TestConfig(unittest.TestCase):
 	def test_prod_config(self):
 		result = self.src_prod
 		self.assertEqual(result.PRODUCTION, True)
+
+	def test_test_config(self):
+		result = self.src_test
+		self.assertTrue(result.MONGO_TEST_USERNAME is not None)
+		self.assertTrue(result.MONGO_TEST_PASSWORD is not None)
+		
