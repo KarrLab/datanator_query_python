c8ec487015edfecc98ec0c95347bf0e3044c03ec
zhouyang.lian <zhouyang.lian@familian.life>
2019-10-04 17:38:17 -0400

    inheriting instead of instantiating

--- tests/query/test_full_text_query.py
+++ tests/query/test_full_text_query.py
@@ -15,13 +15,10 @@ class TestFTX(unittest.TestCase):
         cls.src = full_text_search.FTX(profile_name='es-poweruser', credential_path='~/.wc/third_party/aws_credentials',
                 config_path='~/.wc/third_party/aws_config', elastic_path='~/.wc/third_party/elasticsearch.ini',
                 cache_dir=cls.cache_dir, service_name='es', max_entries=float('inf'), verbose=True)
-        cls.es_manager = es_util.EsUtil(profile_name='es-poweruser', credential_path='~/.wc/third_party/aws_credentials',
-                config_path='~/.wc/third_party/aws_config', elastic_path='~/.wc/third_party/elasticsearch.ini',
-                cache_dir=cls.cache_dir, service_name='es', max_entries=float('inf'), verbose=True)
         cls.index_0 = 'test_0'
         cls.index_1 = 'test_1'
-        cls.url_0 = cls.src.ftx_manager.es_endpoint + '/' + cls.index_0
-        cls.url_1 = cls.src.ftx_manager.es_endpoint + '/' + cls.index_1
+        cls.url_0 = cls.src.es_endpoint + '/' + cls.index_0
+        cls.url_1 = cls.src.es_endpoint + '/' + cls.index_1
         cursor_0 = [{'number': 0, 'mock_key_bulk': 'mock_value_0', 'uniprot_id': 'P0'},
                   {'number': 1, 'mock_key_bulk': 'mock_value_1', 'uniprot_id': 'P1'},
                   {'number': 2, 'mock_key_bulk': 'mock_value_2', 'uniprot_id': 'P2'},
@@ -31,22 +28,22 @@ class TestFTX(unittest.TestCase):
                   {'number': 6, 'mock_key_bulk': 'mock_value_6', 'uniprot_id': 'P6'},
                   {'number': 7, 'mock_key_bulk': 'mock_value_7', 'uniprot_id': 'P7'},
                   {'number': 8, 'mock_key_bulk': 'mock_value_7', 'uniprot_id': 'P8'}]
-        _ = cls.es_manager.data_to_es_bulk(len(cursor_0), cursor_0, cls.index_0, bulk_size=1)
-        _ = cls.es_manager.data_to_es_bulk(len(cursor_0), cursor_1, cls.index_1, bulk_size=1)
+        _ = cls.src.data_to_es_bulk(len(cursor_0), cursor_0, cls.index_0, bulk_size=1)
+        _ = cls.src.data_to_es_bulk(len(cursor_0), cursor_1, cls.index_1, bulk_size=1)
         time.sleep(1)
 
     @classmethod
     def tearDownClass(cls):
         shutil.rmtree(cls.cache_dir)
-        requests.delete(cls.url_0, auth=cls.src.ftx_manager.awsauth)
-        requests.delete(cls.url_1, auth=cls.src.ftx_manager.awsauth)
+        requests.delete(cls.url_0, auth=cls.src.awsauth)
+        requests.delete(cls.url_1, auth=cls.src.awsauth)
 
     def test_request_body_search(self):
         query_0 = 'mock_value_3'
         field_0 = ['mock_key_bulk', 'number']
-        body_0 = self.src.ftx_manager.build_simple_query_string_body(query_0, fields=field_0, lenient=True,
+        body_0 = self.src.build_simple_query_string_body(query_0, fields=field_0, lenient=True,
         analyze_wild_card=True)
         index_0 = 'test_0,test_1'
-        es = self.src.ftx_manager._build_es()
+        es = self.src._build_es()
         es_0 = es.search(index=index_0, body=body_0)
         self.assertEqual(es_0['hits']['hits'][0]['_source'], {'number': 3, 'mock_key_bulk': 'mock_value_3', 'uniprot_id': 'P3'})
