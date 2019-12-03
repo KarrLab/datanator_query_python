04b06f64740295fb008d0ba46fff21f41421ee1d
zhouyang.lian <zhouyang.lian@familian.life>
2019-12-03 12:53:18 -0500

    query_rna_halflife

---
+++ tests/query/test_query_rna_halflife.py
@@ -0,0 +1,32 @@
+from datanator_query_python.query import query_rna_halflife
+from datanator_query_python.config import config
+import unittest
+
+
+class TestQueryRna(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        db = 'datanator'
+        conf = config.TestConfig()
+        username = conf.MONGO_TEST_USERNAME
+        password = conf.MONGO_TEST_PASSWORD
+        MongoDB = conf.SERVER
+        username = username
+        password = password
+        cls.col_str = 'rna_halflife'
+        cls.src = query_rna_halflife.QueryRNA(server=MongoDB, username=username, password=password,
+                                            verbose=True, db=db, collection_str=cls.col_str,
+                                            readPreference='primary')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.src.client.close()
+
+    def test_get_doc_by_oln(self):
+        oln_0 = 'ma0003'
+        docs_0, count_0 = self.src.get_doc_by_oln(oln_0)
+        self.assertEqual(count_0, 1)
+        for doc in docs_0:
+            self.assertEqual(len(doc['halflives']), 2)
+        
