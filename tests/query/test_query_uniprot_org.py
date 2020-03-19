dce905339139248eab337a3d87f2775bd52b2661
Zhouyang Lian <zhouyang.lian@familian.life>
2020-03-19 11:35:45 -0400

    tests/query/test_query_uniprot_org.py

---
+++ tests/query/test_query_uniprot_org.py
@@ -0,0 +1,17 @@
+import unittest
+from datanator_query_python.query import query_uniprot_org
+
+
+class TestQueryUniprotOrg(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.src = query_uniprot_org.QueryUniprotOrg()
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_get_kegg_ortholog(self):
+        result = self.src.get_kegg_ortholog('arCOG00119')
+        print(result)
