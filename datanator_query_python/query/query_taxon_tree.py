88606b20e73122a709193aac9591f93927e3df4c
Zhouyang Lian <zhouyang.lian@familian.life>
2020-03-31 12:52:35 -0400

    use mongo_util in query_taxon_tree

--- datanator_query_python/query/query_taxon_tree.py
+++ datanator_query_python/query/query_taxon_tree.py
@@ -1,12 +1,11 @@
 from datanator_query_python.util import mongo_util, chem_util, file_util
-from . import query_nosql
 import os
 import json
 from pymongo.collation import Collation, CollationStrength
 import pymongo
 
 
-class QueryTaxonTree(query_nosql.DataQuery):
+class QueryTaxonTree(mongo_util.MongoUtil):
     '''Queries specific to taxon_tree collection
     '''
 
@@ -14,15 +13,13 @@ class QueryTaxonTree(query_nosql.DataQuery):
                 verbose=False, max_entries=float('inf'), username=None, MongoDB=None, 
                 password=None, db='datanator', authSource='admin', readPreference='nearest'):
         self.collection_str = collection_str
-        super(query_nosql.DataQuery, self).__init__(cache_dirname=cache_dirname, MongoDB=MongoDB,
-                                        db=db, verbose=verbose, max_entries=max_entries, username=username,
-                                        password=password, authSource=authSource, readPreference=readPreference)
+        super().__init__(cache_dirname=cache_dirname, MongoDB=MongoDB,
+                        db=db, verbose=verbose, max_entries=max_entries, username=username,
+                        password=password, authSource=authSource, readPreference=readPreference)
         self.chem_manager = chem_util.ChemUtil()
         self.file_manager = file_util.FileUtil()
-        self.client, self.db_obj, self.collection = self.con_db(
-            self.collection_str)
+        self.collection = self.db_obj[self.collection_str]
         self.collation = Collation(locale='en', strength=CollationStrength.SECONDARY)
-        self.collection_str = collection_str
 
     def get_all_species(self):
         ''' Get all organisms in taxon_tree collection
