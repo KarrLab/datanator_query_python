feb158317ed7b8087bbe7f267f1d8eaf38d2afb5
Zhouyang <zhouyang.lian@familian.life>
2020-04-30 11:09:15 -0400

    parse_regex in parse_heroku_logs

--- datanator_query_python/util/parse_heroku_logs.py
+++ datanator_query_python/util/parse_heroku_logs.py
@@ -1,3 +1,54 @@
 """Parsing Heroku app datanator-rest-api
 """
 import re
+
+
+class ParseLogs:
+
+    def __init__(self, file_location=None, verbose=True):
+        """Init
+
+        Args:
+            file_location(:obj:`str`): location of log file.
+            verbose(:obj:`bool`): print verbose message.
+        """
+        self.file_location = file_location
+        self.verbose = verbose
+
+    def parse_regex(self, s, regex):
+        """Find in line matches for regulation expressions.
+
+        Args:
+            s(:obj:`str`): String in which to find matching regular expression.
+            regex(:obj:re`): regular expression.
+
+        Return:
+            (:obj:`list`): Matches found. 
+        """
+        return re.search(regex, s)
+
+    def parse_router(self, lines=0):
+        """Parse router lines to get API performance values.
+
+        Args:
+            lines(:obj:`int`, optional): Number of lines to parse. If 0, parse all lines.
+
+        Return:
+            (:obj:`Obj`)
+        """
+        result = {}
+        with open(self.file_location, 'r') as _file:
+            for i, line in enumerate(_file):
+                if lines != 0 and i == lines:
+                    break
+                if line.endswith('\n'):
+                    line = line[0:-1]
+                end_point = re.search(r'(\/.*\w+\/)', line).group(1)
+                performance = int(re.search(r'service=(\d*)ms', line).group(1))
+                if result.get(end_point) is None:
+                    result[end_point] = [performance]
+                else:
+                    result[end_point] += [performance]
+        return result
+
+                
---
+++ tests/util/test_parse_heroku_logs.py
@@ -0,0 +1,60 @@
+from datanator_query_python.util import parse_heroku_logs
+import unittest
+import shutil
+import tempfile
+from pathlib import Path
+
+
+class TestParseLogs(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls._dir = tempfile.mkdtemp()
+        cls.file_location = cls._dir + 'test_logs.txt'
+        s_0 = ("2020-04-29T13:00:15.687982+00:00 heroku[router]:"
+        "at=info method=GET path='/ftx/text_search/num_of_index/?index=metabolites_meta&query_message="
+        "glucose&from_=10&size=10&fields=protein_name&fields=synonyms&fields=enzymes&fields=ko_name&fields="
+        "gene_name&fields=name&fields=enzyme_name&fields=product_names&fields=substrate_names&fields="
+        "enzymes.subunit.canonical_sequence&fields=species' host=api.datanator.info request_id="
+        "9444dc4d-648b-427b-9cf1-1a5f235caacb fwd='23.20.143.235' dyno=web.1 connect=0ms service="
+        "123ms status=200 bytes=418988 protocol=https\n")
+        s_1 = ("2020-04-29T13:00:15.687982+00:00 heroku[router]:"
+        "at=info method=GET path='/something/?index=metabolites_meta&query_message="
+        "glucose&from_=10&size=10&fields=protein_name&fields=synonyms&fields=enzymes&fields=ko_name&fields="
+        "gene_name&fields=name&fields=enzyme_name&fields=product_names&fields=substrate_names&fields="
+        "enzymes.subunit.canonical_sequence&fields=species' host=api.datanator.info request_id="
+        "9444dc4d-648b-427b-9cf1-1a5f235caacb fwd='23.20.143.235' dyno=web.1 connect=0ms service="
+        "200ms status=200 bytes=418988 protocol=https\n")
+        s_2 = ("2020-04-29T13:00:15.687982+00:00 heroku[router]:"
+        "at=info method=GET path='/something/?index=metabolites_meta&query_message="
+        "glucose&from_=10&size=10&fields=protein_name&fields=synonyms&fields=enzymes&fields=ko_name&fields="
+        "gene_name&fields=name&fields=enzyme_name&fields=product_names&fields=substrate_names&fields="
+        "enzymes.subunit.canonical_sequence&fields=species' host=api.datanator.info request_id="
+        "9444dc4d-648b-427b-9cf1-1a5f235caacb fwd='23.20.143.235' dyno=web.1 connect=0ms service="
+        "202ms status=200 bytes=418988 protocol=https")
+        with open(cls.file_location, 'w+') as f:
+            f.writelines([s_0, s_1, s_2])    
+        cls.src = parse_heroku_logs.ParseLogs(file_location=cls.file_location)
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls._dir)
+
+    def test_parse_regex(self):
+        s = ("2020-04-29T13:00:15.687982+00:00 heroku[router]:"
+        "at=info method=GET path='/ftx/text_search/num_of_index/?index=metabolites_meta&query_message="
+        "glucose&from_=10&size=10&fields=protein_name&fields=synonyms&fields=enzymes&fields=ko_name&fields="
+        "gene_name&fields=name&fields=enzyme_name&fields=product_names&fields=substrate_names&fields="
+        "enzymes.subunit.canonical_sequence&fields=species' host=api.datanator.info request_id="
+        "9444dc4d-648b-427b-9cf1-1a5f235caacb fwd='23.20.143.235' dyno=web.1 connect=0ms service="
+        "123ms status=200 bytes=418988 protocol=https")
+        regex_0 = r'(\/.*\w+\/)'
+        regex_1 = r'service=(\d*)ms'
+        result_0 = self.src.parse_regex(s, regex_0)
+        result_1 = self.src.parse_regex(s, regex_1)
+        self.assertEqual(result_0.group(0), "/ftx/text_search/num_of_index/")
+        self.assertEqual(result_1.group(1), "123")
+
+    def test_parse_router(self):
+        result = self.src.parse_router()
+        self.assertEqual(result, {'/ftx/text_search/num_of_index/': [123], '/something/': [200, 202]})
