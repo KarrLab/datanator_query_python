2191539f00edbef7d642ce05580729fc92b3d7dd
Zhouyang Lian <zhouyang.lian@familian.life>
2020-05-18 17:42:36 -0400

    added pipelines.py

---
+++ datanator_query_python/aggregate/pipelines.py
@@ -0,0 +1,11 @@
+"""Various aggregation pipelines
+"""
+
+
+class Pipeline:
+    def __init__(self):
+        pass
+
+    def aggregate_kegg_orthology(self):
+        """Aggregate kegg orthology information
+        """
