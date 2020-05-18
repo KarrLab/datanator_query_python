f77c534f8dc7dd79d9f9108d3b4f03e4e1af027b
Zhouyang Lian <zhouyang.lian@familian.life>
2020-05-18 17:47:49 -0400

    added test_pipelines

--- datanator_query_python/aggregate/pipelines.py
+++ datanator_query_python/aggregate/pipelines.py
@@ -8,4 +8,19 @@ class Pipeline:
 
     def aggregate_kegg_orthology(self):
         """Aggregate kegg orthology information
+
+        Return:
+            (:obj:`list`)
         """
+        return 
+        [{"$match":
+            {"$expr":
+                {"$and":
+                    [
+                        {"$eq": [ "$stock_item",  "$$order_item" ] },
+                        {"$gte": [ "$instock", "$$order_qty" ] }
+                    ]
+                }
+            }
+        },
+        {"$project": {"stock_item": 0, "_id": 0}}]
