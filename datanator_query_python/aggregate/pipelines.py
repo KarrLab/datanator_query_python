0d04f31b4dbcf885a9234e797fdb2e17bb585ff1
Zhouyang Lian <zhouyang.lian@familian.life>
2020-05-19 11:18:51 -0400

    lookup.py

--- datanator_query_python/aggregate/pipelines.py
+++ datanator_query_python/aggregate/pipelines.py
@@ -6,22 +6,17 @@ class Pipeline:
     def __init__(self):
         pass
 
-    def aggregate_kegg_orthology(self):
+    def aggregate_kegg_orthology(self, expr, projection={'_id': 0, 'gene_ortholog': 0}):
         """Aggregate kegg orthology information
         
         Args:
+            expr(:obj:`Obj`): match expression.
+            projection(:obj:`Obj`, optional): projection in pipeline.
 
         Return:
             (:obj:`list`)
         """
         return [{"$match":
-            {"$expr":
-                {"$and":
-                    [
-                        {"$eq": [ "$stock_item",  "$$order_item" ] },
-                        {"$gte": [ "$instock", "$$order_qty" ] }
-                    ]
-                }
-            }
-        },
-        {"$project": {"stock_item": 0, "_id": 0}}]
+                    {"$expr": expr}
+                },
+                {"$project": projection}]
--- tests/aggregate/test_pipelines.py
+++ tests/aggregate/test_pipelines.py
@@ -13,5 +13,11 @@ class TestPipelines(unittest.TestCase):
         pass
 
     def test_aggregate_kegg_orthology(self):
-        result = self.src.aggregate_kegg_orthology()
-        self.assertEqual(result[1], {"$project": {"stock_item": 0, "_id": 0}})
+        expr = {"$and":
+                    [
+                        {"$eq": [ "$stock_item", "$$order_item"]},
+                        {"$gte": ["$instock", "$$order_qty"]}
+                    ]
+               }
+        result = self.src.aggregate_kegg_orthology(expr)
+        self.assertEqual(result[1], {"$project": {'_id': 0, 'gene_ortholog': 0}})
