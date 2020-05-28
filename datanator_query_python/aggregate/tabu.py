09a642149df170b343d6fea4db7fb80d643646a4
Zhouyang <zhouyang.lian@familian.life>
2020-05-28 17:58:18 -0400

    taxon tabulation complete https://github.com/KarrLab/datanator_rest_api/issues/97

--- datanator_query_python/aggregate/tabu.py
+++ datanator_query_python/aggregate/tabu.py
@@ -15,10 +15,6 @@ class Tabu(mongo_util.MongoUtil):
         self.verbose = verbose
         self.max_entries = max_entries
         self.uniprot_col = self.db_obj['uniprot']
-        self.sabiork_col = self.db_obj['sabio_rk_old']
-        self.rna_half_col = self.db_obj['rna_halflife_new']
-        self.rna_mod_col = self.db_obj['rna_modification']
-        self.meta_con_col = self.db_obj['metabolite_concentrations']
         self.pipeline_manager = pipelines.Pipeline()
 
     def uniprot_taxon_dist(self):
@@ -50,6 +46,10 @@ class Tabu(mongo_util.MongoUtil):
 
 
 from datanator_query_python.config import config
+import itertools
+from collections import defaultdict
+import json
+
 
 def main():
     db = 'datanator'
@@ -62,3 +62,37 @@ def main():
                readPreference='nearest', max_entries=10)
 
     uniprot_docs = src.uniprot_taxon_dist()
+    sabio_docs = src.taxon_dist("sabio_rk_old", "taxon_id", match={"$match": {"taxon_id": {"$ne": None}}})
+    rnamod_docs = src.taxon_dist("rna_modification", "modifications.ncbi_taxonomy_id", unwind={"$unwind": "$modifications"}, 
+                                 match={"$match": {"modifications.ncbi_taxonomy_id": {"$ne": None}}})
+    rnahalf_docs = src.taxon_dist("rna_halflife_new", "halflives.ncbi_taxonomy_id", unwind={"$unwind": "$halflives"})
+    conc_docs = src.taxon_dist("metabolite_concentrations", "concentrations.ncbi_taxonomy_id", unwind={"$unwind": "$concentrations"})
+
+    obj = defaultdict(int)
+    for (p_doc, s_doc, m_doc, l_doc, c_doc) in itertools.zip_longest(uniprot_docs, sabio_docs, rnamod_docs, rnahalf_docs, conc_docs, fillvalue={'_id': 1, 'count': 0}):
+        obj[p_doc['_id']] += p_doc['count']
+        obj[s_doc['_id']] += s_doc['count']
+        obj[m_doc['_id']] += m_doc['count']
+        obj[l_doc['_id']] += l_doc['count']
+        obj[c_doc['_id']] += c_doc['count']
+
+    # sort obj
+    new_obj = {k: v for k, v in sorted(obj.items(), key=lambda item: item[1], reverse=True)}
+
+    with open("./docs/taxon_distribution.json", "w+") as outfile: 
+        json.dump(new_obj, outfile)
+
+    final_obj = {"others": 0}
+    for i, (key, val) in enumerate(new_obj.items()):
+        if i < 11 and key != 1:
+            name = src.db_obj['taxon_tree'].find_one({"tax_id": int(key)}, projection={"tax_name": 1})["tax_name"]
+            final_obj[name] = val
+        else:
+            final_obj["others"] += val 
+
+    with open("./docs/taxon_distribution_frontend.json", "w+") as outfile: 
+        json.dump(final_obj, outfile)
+
+
+if __name__ == '__main__':
+    main()
