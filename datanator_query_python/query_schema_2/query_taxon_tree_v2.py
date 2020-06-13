61626c7bdffd4148cc0aced0f2e18edec1e5f597
Zhouyang <zhouyang.lian@familian.life>
2020-06-12 20:53:52 -0400

    get_canon_ancestor

--- datanator_query_python/query_schema_2/query_taxon_tree_v2.py
+++ datanator_query_python/query_schema_2/query_taxon_tree_v2.py
@@ -1,7 +1,34 @@
-from datanator_query_python.config import query_schema_2_manager
+from datanator_query_python.config import query_schema_2_manager, config
 
 
 class QTaxon(query_schema_2_manager.QM):
 
-    def __init__(self):
-        pass
+    def __init__(self,
+                username=config.Config.USERNAME,
+                password=config.Config.PASSWORD,
+                readPreference=config.Config.READ_PREFERENCE,
+                collection='taxon_tree',
+                db='datanator-test',
+                max_entries=float('inf')):
+        super().__init__(username=username, password=password)
+        self.collection = self.client.get_database(db,
+                                                   read_preference=self.read_preference)[collection]
+
+    def get_canon_ancestor(self, _id, _format='tax_id'):
+        """Get organism's canon ancestor information.
+        
+        Args:
+            _id(:obj:`int` or :obj:`str`): identification for organism.
+            format(:obj:`str`, optional): identification format, i.e. tax_id or tax_name.
+
+        Return:
+            (:obj:`tuple` of :obj:`list` of :obj:`int` and :obj:`list` of :obj:`str`): canon_anc_id, canon_anc_name
+        """
+        query = {_format: _id}
+        doc = self.collection.find_one(filter=query, 
+                                 projection={"canon_anc_names": 1,
+                                             "canon_anc_ids": 1})
+        if doc is None:
+            return [], []
+        else:
+            return doc.get("canon_anc_ids", []), doc.get("canon_anc_names", [])
--- tests/query_schema_2/test_query_taxon_tree_v2.py
+++ tests/query_schema_2/test_query_taxon_tree_v2.py
@@ -1,4 +1,5 @@
 import unittest
+from datanator_query_python.config import config as query_config
 from datanator_query_python.query_schema_2 import query_taxon_tree_v2
 
 
@@ -6,8 +7,18 @@ class TestQTaxon(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        pass
+        cls.src = query_taxon_tree_v2.QTaxon(username=query_config.TestConfig.MONGO_TEST_USERNAME,
+                                             password=query_config.TestConfig.MONGO_TEST_PASSWORD)
 
     @classmethod
     def tearDownClass(cls):
-        pass
+        cls.src.client.close()
+
+    def test_get_canon_ancestor(self):
+        _id = 1915648
+        ids, _ = self.src.get_canon_ancestor(_id, _format='tax_id')
+        self.assertTrue(2283796 not in ids)
+        self.assertTrue(2157 in ids)
+        _id = "nonsense"
+        ids, _ = self.src.get_canon_ancestor(_id, _format='tax_name')
+        self.assertEqual([], ids)
