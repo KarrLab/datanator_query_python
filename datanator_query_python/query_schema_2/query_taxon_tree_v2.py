ce9f1e91226dab4f97af8f40b4307e639a1941fa
Zhouyang Lian <zhouyang.lian@familian.life>
2020-06-14 15:34:19 -0400

    aggregate_distance

--- datanator_query_python/query_schema_2/query_taxon_tree_v2.py
+++ datanator_query_python/query_schema_2/query_taxon_tree_v2.py
@@ -1,4 +1,5 @@
 from datanator_query_python.config import query_schema_2_manager, config
+from collections import deque 
 
 
 class QTaxon(query_schema_2_manager.QM):
@@ -32,3 +33,41 @@ class QTaxon(query_schema_2_manager.QM):
             return [], []
         else:
             return doc.get("canon_anc_ids", []), doc.get("canon_anc_names", [])
+
+    def aggregate_distance(self, docs, target_org, _format='tax_id',
+                          name_field='species_name'):
+        """Aggregate distance information between organisms in documents and target
+        organism. 
+        e.g. 1) org_0 has ancestors [a, b, c, d, e]; target_org has ancestors 
+        [a, b, c, f, g, h]  -> org_0_dist = 2 (e,d), target_org_dist = 3 (h,g,f).
+        2) org_0 has ancestors [a, b, c, d, e]; target_org is d -> target_org_dist=0.
+
+        Args:
+            docs(:obj:`list` of :obj:`Obj`): list of docs to be compared with target org.
+            target_org (:obj:`int` or :obj:`str`): identification of the target organism.
+            _format (:obj:`str`, optional): identification format. Defaults to 'tax_id'.
+            name_field (:obj:`str`, optional): Field where species name is in each doc. Defaults to 'species_name'.
+
+        Return:
+            (:obj:`list` of :obj:`Obj`): Objects containing information on distance to target_org.
+        """
+        # find target organism document
+        target_org_doc = self.collection.find_one(filter={_format: target_org},
+                                                  projection={"canon_anc_ids": 1, "canon_anc_names": 1,
+                                                              "tax_name": 1})
+        if target_org_doc is None:
+            return docs
+        else:
+            target_org_name = target_org_doc['tax_name']
+            target_org_anc_ids = target_org_doc['canon_anc_ids']
+            target_org_anc_ids_set = set(target_org_anc_ids)
+            target_org_anc_names = target_org_doc['canon_anc_names']
+            for doc in docs:
+                measured = doc['canon_anc_ids']
+                intersect = len(set(measured).intersection(target_org_anc_ids_set))
+                measured_dist = len(measured) - intersect
+                target_dist = len(target_org_anc_ids) - intersect
+                doc['taxon_distance'] = {doc[name_field]: measured_dist, 
+                                         target_org_name: target_dist,
+                                         "{}_canon_ancestors".format(target_org_name): target_org_anc_names}
+            return docs
--- tests/query_schema_2/test_query_taxon_tree_v2.py
+++ tests/query_schema_2/test_query_taxon_tree_v2.py
@@ -1,6 +1,7 @@
 import unittest
 from datanator_query_python.config import config as query_config
 from datanator_query_python.query_schema_2 import query_taxon_tree_v2
+from pprint import pprint
 
 
 class TestQTaxon(unittest.TestCase):
@@ -22,3 +23,20 @@ class TestQTaxon(unittest.TestCase):
         _id = "nonsense"
         ids, _ = self.src.get_canon_ancestor(_id, _format='tax_name')
         self.assertEqual([], ids)
+
+    def test_aggregate_distance(self):
+        measured_0 = [{"canon_anc_ids": [131567, 2, 1224, 1236, 91347, 543, 590, 28901],
+                       "tax_name": "Salmonella enterica subsp. enterica serovar Newport str. CFSAN000907"}] #tax_id1299189
+        target_0 = 0
+        target_1 = 1227178
+        result_0 = self.src.aggregate_distance(measured_0, target_0, name_field='tax_name')
+        self.assertEqual(result_0, measured_0)
+        result_1 = self.src.aggregate_distance(measured_0, target_1, name_field='tax_name')
+        self.assertEqual(result_1[0]['taxon_distance']['Salmonella enterica subsp. enterica serovar Newport str. CFSAN001557'], 0)
+        # taget is measured's ancestor
+        measured_1 = [{"canon_anc_ids": [131567, 2759, 4751, 4890, 4891, 4892, 4893, 4930, 4932],
+                       "tax_name": "Saccharomyces cerevisiae CAT-1"}]
+        target_2 = 4932  # Saccharomyces cerevisiae
+        result_2 = self.src.aggregate_distance(measured_1, target_2, name_field='tax_name')
+        self.assertEqual(result_2[0]['taxon_distance']['Saccharomyces cerevisiae'], 0)
+        self.assertEqual(result_2[0]['taxon_distance']['Saccharomyces cerevisiae CAT-1'], 1)
