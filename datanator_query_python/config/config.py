8596f7c49a415d16ec64eac7c08e9884fe620ed4
Zhouyang <zhouyang.lian@familian.life>
2020-07-21 15:35:46 -0400

    patch master config

--- datanator_query_python/config/config.py
+++ datanator_query_python/config/config.py
@@ -82,11 +82,11 @@ class FtxConfig(Config):
     REST_FTX_AWS_PROFILE = os.getenv("REST_FTX_AWS_PROFILE")
 
 
-class SchemaMigration:
+class SchemaMigration(Config):
     USERNAME=os.getenv('MONGO_DATANATOR_PROD')
     PASSWORD=os.getenv('MONGO_DATANATOR_PROD_PASSWORD')
 
 
-class DatanatorTest:
+class DatanatorTest(Config):
     USERNAME=os.getenv('MONGO_DATANATOR_TEST')
     PASSWORD=os.getenv('MONGO_DATANATOR_TEST_PASSWORD')
