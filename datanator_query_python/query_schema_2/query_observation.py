4b8b2b1877fc98c9805f98d830f247fcd1206a54
Zhouyang <zhouyang.lian@familian.life>
2020-08-10 10:58:05 -0400

    get_protein_datatype -> get_entity_datatype

--- datanator_query_python/query_schema_2/query_observation.py
+++ datanator_query_python/query_schema_2/query_observation.py
@@ -8,18 +8,20 @@ class QueryObs(query_schema_2_manager.QM):
         super().__init__()
         self.db = db
 
-    def get_protein_datatype(self, 
+    def get_entity_datatype(self, 
                               identifier,
+                              entity="protein",
                               datatype="half-life",
                               collection="observation",
                               limit=10,
                               skip=0,
                               projection={"_id": 0}):
-        """Get protein datatype.
+        """Get entity datatype.
 
         Args:
-            identifier(:obj:`Obj`): identifier used for the protein.
-            datatype(:obj:`Obj`): Datatype to be retrieved.
+            identifier(:obj:`Obj`): identifier used for the entity.
+            entity(:obj:`Obj`, optional): entity type. i.e. "protein", "RNA", etc. 
+            datatype(:obj:`Obj`, optional): Datatype to be retrieved.
             collection(:obj:`str`, optional): name of collection in which values reside.
             limit(:obj:`int`, optional): number of results to return.
             skip(:obj:`int`, optional): number of documents to skip.
@@ -29,13 +31,15 @@ class QueryObs(query_schema_2_manager.QM):
         """
         results = []
         col = self.client[self.db][collection]
-        con_0 = {"entity.type": "protein"}
+        con_0 = {"entity.type": entity}
         con_1 = {}
-        if datatype != "localization":
+        if entity == "protein" and datatype != "localization":
             con_1["values.type"] = datatype
-        else:
+        elif entity == "protein" and datatype == "half-life":
             words = ["intramembrane_localization", "secretome location"]
             con_1["values.type"] = {"$in": words}
+        elif entity == "RNA" and datatype == "localization":
+            con_1["values.type"] = "subcellular_localization"
         query = {"$and": [{"identifier": identifier}, con_0, con_1]}
         docs = col.find(filter=query, limit=limit, skip=skip,
                         collation=self.collation,
--- tests/query_schema_2/test_query_observation.py
+++ tests/query_schema_2/test_query_observation.py
@@ -12,13 +12,16 @@ class TestQOb(unittest.TestCase):
     def tearDownClass(cls):
         cls.src.client.close()
 
-    def test_get_protein_datatype(self):
+    def test_get_entity_datatype(self):
         identfier = {"namespace": "gene_symbol", "value": "BAG1"}
-        results = self.src.get_protein_datatype(identfier)
+        results = self.src.get_entity_datatype(identfier)
         self.assertEqual(results[0]["entity"]["name"], 'BCL2-associated athanogene.')
         identfier = {"namespace": "gene_symbol", "value": "something"}
-        results = self.src.get_protein_datatype(identfier)
+        results = self.src.get_entity_datatype(identfier)
         self.assertEqual(results, [])
         identfier = {"namespace": "gene_name", "value": "EMC3"}
-        results = self.src.get_protein_datatype(identfier, datatype="localization")
+        results = self.src.get_entity_datatype(identfier, datatype="localization")
         self.assertEqual(results[0]["entity"]["name"], 'ER membrane protein complex subunit 3')
+        identfier = {"namespace": "gene_id", "value": "100003563"}
+        results = self.src.get_entity_datatype(identfier, entity="RNA", datatype="localization")
+        self.assertEqual(results[0]["genotype"]["taxon"]["name"], 'Danio rerio')
