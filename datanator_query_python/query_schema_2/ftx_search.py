6cc715bcb1f7665f740272a05afbe0d8c4981c27
Zhouyang <zhouyang.lian@familian.life>
2020-08-12 14:23:14 -0400

    test_ftx_search.py

--- datanator_query_python/query_schema_2/ftx_search.py
+++ datanator_query_python/query_schema_2/ftx_search.py
@@ -9,15 +9,41 @@ class FTX(query_schema_2_manager.QM):
                      msg,
                      skip=0,
                      limit=10,
+                     token_order="sequential",
                      db="datanator-test"):
         """Search for taxon names.
+        (https://docs.atlas.mongodb.com/reference/atlas-search)
 
         Args:
             msg(:obj:`str`): query message.
-            skip(:obj:`int`): number of records to skip.
-            limit(:obj:`int`): max number of documents to return.
+            skip(:obj:`int`, optional): number of records to skip.
+            limit(:obj:`int`, optional): max number of documents to return.
+            token_order(:obj:`str`, optional): token order, i.e. sequential or any.
             db(:obj:`str`, optional): name of database in which the result resides.
 
         Return:
             (:obj:`CommandCursor`): MongDB CommandCursor after aggregation.
         """
+        collection = self.client[db]["taxon_tree"]
+        path = ["tax_name", "name_txt"]
+        return collection.aggregate([
+                                        {
+                                            "$search": {
+                                                "autocomplete": {
+                                                    "path": path,
+                                                    "query": msg,
+                                                    "tokenOrder": token_order
+                                                }
+                                            }
+                                        },
+                                        {
+                                            "$limit": limit,
+                                            "$skip": skip
+                                        },
+                                        {
+                                            "$project": {
+                                                "_id": 0,
+                                                "tax_name": 1
+                                            }
+                                        }
+                                    ])
